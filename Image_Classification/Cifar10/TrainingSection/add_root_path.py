51693a7f3f72dce0bc281a189204eac3d7d3160d
JiaenSuen <john20050826@gmail.com>
2025-10-13 19:17:12 +0800

    r

--- Image_Classification/Cifar10/TrainingSection/add_root_path.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-def add_root_to_syspath(levels_up=1):
-    path = os.path.abspath(__file__)
-    for _ in range(levels_up):
-        path = os.path.dirname(path)
-    if path not in sys.path:
-        sys.path.insert(0, path)
