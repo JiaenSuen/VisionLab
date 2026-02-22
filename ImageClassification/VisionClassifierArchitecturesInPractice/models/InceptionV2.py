bb72f6307d555b3467fb06650709d24a56e0f46d
JiaenSuen <john20050826@gmail.com>
2026-02-22 23:27:10 +0800

    InceptionV2

--- ImageClassification/VisionClassifierArchitecturesInPractice/models/InceptionV2.py
+++ ImageClassification/VisionClassifierArchitecturesInPractice/models/InceptionV2.py
@@ -10,127 +10,141 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def build_googlenet2(num_classes=1000):
-    return InceptionNetV2(num_classes=num_classes)
+def build_inception2(num_classes=1000):
+    return InceptionV2(num_classes=num_classes)
 
-class InceptionNetV2(nn.Module):
+class InceptionV2(nn.Module):
     def __init__(self, num_classes=1000):
-        super(InceptionNetV2, self).__init__()
+        super().__init__()
 
-        self.conv1 = conv_block(3, 64, kernel_size=7, stride=2, padding=3)
-        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.stem = Stem()
 
-        self.conv2 = conv_block(64, 64, kernel_size=1)
-        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.in3a = InceptionV2Block(192, 64, 64, 64, 64, 96, 32)
+        self.in3b = InceptionV2Block(256, 64, 64, 96, 64, 96, 64)
 
-        self.inception3a = InceptionV2_block(64, 64, 96, 128, 16, 32, 32)
-        self.inception3b = InceptionV2_block(256, 128, 128, 192, 32, 96, 64) 
-        self.downsample3 = Inception_downsample(480, 480)   # V2
+        self.reduction3 = Reduction(320, 128, 160, 64, 96)
 
-        self.inception4a = InceptionV2_block(480, 192, 96 , 208, 16, 48, 64)
-        self.inception4b = InceptionV2_block(512, 160, 112, 224, 24, 64, 64)
-        self.inception4c = InceptionV2_block(512, 128, 128, 256, 24, 64, 64)
-        self.inception4d = InceptionV2_block(512, 112, 144, 288, 32, 64, 64)
-        self.inception4e = InceptionV2_block(528, 256, 160, 320, 32, 128, 128)
-        self.downsample4 = Inception_downsample(832, 832)  # V2
+        self.in4a = InceptionV2Block(576, 224, 64, 96, 96, 128, 128)
+        self.in4b = InceptionV2Block(576, 192, 96, 128, 96, 128, 128)
 
-        self.inception5a = InceptionV2_block(832, 256, 160, 320, 32, 128, 128)
-        self.inception5b = InceptionV2_block(832, 384, 192, 384, 48, 128, 128)
+        self.reduction4 = Reduction(576, 128, 192, 128, 192)
 
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.dropout = nn.Dropout(p=0.4)
+        self.in5a = InceptionV2Block(960, 352, 192, 320, 160, 224, 128)
+        self.in5b = InceptionV2Block(1024, 352, 192, 320, 192, 224, 128)
+
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.dropout = nn.Dropout(0.2)
         self.fc = nn.Linear(1024, num_classes)
 
-    def forward(self, x):  
-        x = self.conv1(x)
-        x = self.maxpool1(x)
+    def forward(self, x):
+        x = self.stem(x)
 
-        x = self.conv2(x)
-        x = self.maxpool2(x)
+        x = self.in3a(x)
+        x = self.in3b(x)
+        x = self.reduction3(x)
 
-        x = self.inception3a(x)
-        x = self.inception3b(x)
-        x = self.downsample3(x)
+        x = self.in4a(x)
+        x = self.in4b(x)
+        x = self.reduction4(x)
 
-        x = self.inception4a(x)
-        x = self.inception4b(x)
-        x = self.inception4c(x)
-        x = self.inception4d(x)
-        x = self.inception4e(x)
-        x = self.downsample4(x)
-        x = self.inception5a(x)
-        x = self.inception5b(x)
+        x = self.in5a(x)
+        x = self.in5b(x)
+
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.dropout(x)
-        x = self.fc(x)
-        return x
+        return self.fc(x)
 
 
 
-
-class InceptionV2_block(nn.Module):
-    def __init__(self, in_channels, out_1x1, red_3x3, out_3x3, red_5x5, out_5x5, out_pool):
-        super(InceptionV2_block,self).__init__()
-        self.branch1 = conv_block(in_channels, out_1x1, kernel_size=1)
-        self.branch2 = nn.Sequential(
-            conv_block(in_channels, red_3x3, kernel_size=1),
-            conv_block(red_3x3, out_3x3, kernel_size=3, padding=1)
-        )
-        self.branch3 = nn.Sequential(   # V2
-            conv_block(in_channels, red_5x5, kernel_size=1),
-            conv_block(red_5x5, out_5x5, kernel_size=3, padding=1),
-            conv_block(out_5x5, out_5x5, kernel_size=3, padding=1)
-        )
-        self.branch4 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            conv_block(in_channels, out_pool, kernel_size=1)
-        )
-    def forward(self, x):
-        branch1 = self.branch1(x)
-        branch2 = self.branch2(x)
-        branch3 = self.branch3(x)
-        branch4 = self.branch4(x)
-        outputs = [branch1, branch2, branch3, branch4]
-        return torch.cat(outputs, 1)
-
-
-class conv_block(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
-        super(conv_block,self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        return x
-    
-# V2 Update : DownSampling Module
-class Inception_downsample(nn.Module):
-    def __init__(self, in_channels, out_channels):
+class InceptionV2Block(nn.Module):
+    def __init__(self, in_ch, c1, c3r, c3, c5r, c5, pool):
         super().__init__()
 
-        self.branch1 = conv_block(
-            in_channels,
-            out_channels // 2,
-            kernel_size=3,
-            stride=2,
-            padding=1  
+        self.branch1 = ConvBN(in_ch, c1, 1)
+
+        self.branch2 = nn.Sequential(
+            ConvBN(in_ch, c3r, 1),
+            ConvBN(c3r, c3, 3, 1, 1)
         )
 
-        self.branch2 = conv_block(
-            in_channels,
-            out_channels // 2,
-            kernel_size=3,
-            stride=2,
-            padding=1 
+        # 5x5 -> 3x3 + 3x3
+        self.branch3 = nn.Sequential(
+            ConvBN(in_ch, c5r, 1),
+            ConvBN(c5r, c5, 3, 1, 1),
+            ConvBN(c5, c5, 3, 1, 1)
+        )
+
+        self.branch4 = nn.Sequential(
+            nn.AvgPool2d(3, 1, 1),
+            ConvBN(in_ch, pool, 1)
         )
 
     def forward(self, x):
         return torch.cat([
             self.branch1(x),
-            self.branch2(x)
+            self.branch2(x),
+            self.branch3(x),
+            self.branch4(x)
+        ], 1)
+
+
+
+class ConvBN(nn.Module):
+    def __init__(self, in_ch, out_ch, k, s=1, p=0):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, k, s, p, bias=False),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class Stem(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.stem = nn.Sequential(
+            ConvBN(3, 32, 3, 2, 1),
+            ConvBN(32, 32, 3, 1, 1),
+            ConvBN(32, 64, 3, 1, 1),
+            nn.MaxPool2d(3, 2, 1),
+            ConvBN(64, 80, 1),
+            ConvBN(80, 192, 3, 1, 1),
+            nn.MaxPool2d(3, 2, 1)
+        )
+
+    def forward(self, x):
+        return self.stem(x)
+
+
+
+# V2 Update : DownSampling Module
+class Reduction(nn.Module):
+    def __init__(self, in_ch, k, l, m, n):
+        super().__init__()
+
+        # 1x1 -> 3x3 stride=2
+        self.branch1 = nn.Sequential(
+            ConvBN(in_ch, k, 1),
+            ConvBN(k, l, 3, 2, 1)
+        )
+
+        # 1x1 -> 3x3 -> 3x3 stride=2
+        self.branch2 = nn.Sequential(
+            ConvBN(in_ch, m, 1),
+            ConvBN(m, n, 3, 1, 1),
+            ConvBN(n, n, 3, 2, 1)
+        )
+
+        # pooling branch
+        self.branch3 = nn.MaxPool2d(3, 2, 1)
+
+    def forward(self, x):
+        return torch.cat([
+            self.branch1(x),
+            self.branch2(x),
+            self.branch3(x)
         ], 1)
--- ImageClassification/VisionClassifierArchitecturesInPractice/models/_modelRouter.py
+++ ImageClassification/VisionClassifierArchitecturesInPractice/models/_modelRouter.py
@@ -1,13 +1,13 @@
 from .ResNetV1    import build_resnet18pt,build_resnet34pt
 from .GoogLeNet    import build_googlenet,build_googlenet_pt
-from .InceptionV2  import build_googlenet2
+from .InceptionV2  import build_inception2
 from .HighwayNet   import build_HighwayNet
  
 modelRouter = {
     "googlenet"   : build_googlenet,
     "googlenetpt" : build_googlenet_pt,
 
-    "inceptionv2" : build_googlenet2,
+    "inceptionv2" : build_inception2,
 
     "highwaynet23" : build_HighwayNet,
 
